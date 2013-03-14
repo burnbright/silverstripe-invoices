aeb1fa136f7fd7bc1e1b4a0ef20c6a5ffaa72a4e
Jeremy Shipman <jeremy@burnbright.net>
2013-03-15 10:58:48 +1300

    Removed payment CMS fields changes in InvoicePaymentDecorator

--- code/decorators/InvoicePaymentDecorator.php
+++ code/decorators/InvoicePaymentDecorator.php
@@ -6,14 +6,6 @@ class InvoicePaymentDecorator extends DataExtension {
 	
 	function extraStatics($class = null, $extension = null) {
 		
-		//Customise model admin summary fields
-		//warning - can't do this because it affects DataObject
-		
-		//Payment::$summary_fields['ID'] = 'ID';
-		//Payment::$summary_fields['Created'] = 'Created';
-		//Payment::$summary_fields['ClassName'] = 'Type';
-		//Payment::$summary_fields['Status'] = 'Status';
-		
 		Payment::$searchable_fields = array(
 			'ID','ClassName'
 		);
@@ -27,11 +19,4 @@ class InvoicePaymentDecorator extends DataExtension {
 		);
 	}
 	
-	function updateCMSFields(FieldList $fields){
-		
-		$fields->addFieldToTab('Root.Main',new PopupDateTimeField('Created','Date'));
-		
-	}
-	
 }
-?>
